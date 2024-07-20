7daef1c775e89614f495be9653ea322bbf82baa0
TorenHF <toren@aduki.co.uk>
2024-07-20 11:37:43 +0200

    NN built, but not intergrated into the MCTS

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,6 +3,8 @@ import torch
 import math
 import torch.nn as nn
 import torch.nn.functional as F
+torch.manual_seed(0)
+
 class tictactoe:
     def __init__(self):
         self.row_count = 3
@@ -74,7 +76,7 @@ class RestNet(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(32 * game.row_count * game.column_count, game.action_size)
+            nn.Linear(32 * game.row_count * game.colum_count, game.action_size)
         )
 
         self.valueHead =nn.Sequential(
@@ -82,7 +84,7 @@ class RestNet(nn.Module):
             nn.BatchNorm2d(3),
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(3 * game.row_count * game.column_count, 1),
+            nn.Linear(3 * game.row_count * game.colum_count, 1),
             nn.Tanh()
         )
 
@@ -114,20 +116,21 @@ class ResBlock(nn.Module):
 
 
 class Node:
-    def __init__(self, game, args, state, parent=None, action_taken=None):
+    def __init__(self, game, args, state, parent=None, action_taken=None, prior=0):
         self.game = game
         self.args = args
         self.state = state
         self.parent = parent
         self.action_taken = action_taken
+        self.prior = prior
 
         self.children = []
-        self.expandable_moves = game.get_valid_moves(state)
+        #self.expandable_moves = game.get_valid_moves(state), removing this, but actually could be used to make search phase more effficient
         self.visit_count = 0
         self.value_sum = 0
 
     def is_fully_expanded(self):
-        return np.sum(self.expandable_moves) == 0 and len(self.children) > 0
+        return len(self.children) > 0
 
     def select(self):
         best_child = None
@@ -141,18 +144,21 @@ class Node:
         return best_child
 
     def get_ucb(self, child):
-        q_value = 1 - ((child.value_sum / child.visit_count) + 1) / 2
-        return q_value + self.args['C'] * math.sqrt(math.log(self.visit_count) / child.visit_count)
+        if child.visit_count == 0:
+            q_value = 0
+        else:
+            q_value = 1 - ((child.value_sum / child.visit_count) + 1) / 2
+        return q_value + self.args['C'] * (math.sqrt(self.visit_count) / (1 + child.visit_count)) * child.prior
 
-    def expand(self):
-        action = np.random.choice(np.where(self.expandable_moves == 1)[0])
-        self.expandable_moves[action] = 0
-        child_state = self.state.copy()
-        child_state = self.game.get_next_state(child_state, action, 1)
-        child_state = self.game.change_perspective(child_state, player=-1)
+    def expand(self, policy):
+        for action, prob in enumerate(policy):
+            if prob > 0:
+                child_state = self.state.copy()
+                child_state = self.game.get_next_state(child_state, action, 1)
+                child_state = self.game.change_perspective(child_state, player=-1)
 
-        child = Node(self.game, self.args, child_state, self, action)
-        self.children.append(child)
+                child = Node(self.game, self.args, child_state, self, action, prob)
+                self.children.append(child)
         return child
 
     def simulate(self):
@@ -184,10 +190,12 @@ class Node:
 
 
 class MCTS:
-    def __init__(self, game, args):
+    def __init__(self, game, args, model):
         self.game = game
         self.args = args
+        self.model = model
 
+    @torch.no_grad()
     def search(self, state):
         root = Node(self.game, self.args, state)
 
@@ -201,8 +209,18 @@ class MCTS:
             value = self.game.get_opponent_value(value)
 
             if not is_terminal:
-                node = node.expand()
-                value = node.simulate()
+                policy, value = self.model(
+                    torch.tensor(self.game.get_encoded_state(node.state)).unsqueeze(0)
+                )
+                policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy()
+                valid_moves = self.game.get_valid_moves(node.state)
+                policy *= valid_moves
+                policy /= np.sum(policy)
+
+                value = value.item()
+
+                node.expand(policy)
+
 
             node.backpropagate(value)
 
@@ -211,17 +229,46 @@ class MCTS:
             action_probs[child.action_taken] = child.visit_count
         action_probs /= np.sum(action_probs)
         return action_probs
+class AlphaZero:
+    def __init__(self, model, optimizer, game, args):
+        self.model = model
+        self.optimizer = optimizer
+        self.game = game
+        self.args = args
+        self.mcts = MCTS(game, args, model)
 
+    def selfPlay(self):
+        pass
+
+    def train(self):
+        pass
+
+    def learn(self):
+        for iteration in self.args['num_iterations']:
+            memory = []
+
+            self.model.eval()
+            for selfPlay_iteration in range(self.args['num_selfPlay_iterations']):
+                memory += self.selfPlay()
+
+            self.model.train()
+            for epoch in range(self.args['num_epochs']):
+                self.train(memory)
+
+            torch.save(self.model.state_dict(), f"model_{iteration}.pt" )
+            torch.save(self.optimizer.)
 
 game = tictactoe()
 player = 1
 state = game.get_init_state()
 
 args = {
-    'C': 1.41,
+    'C': 2,
     'num_searches': 1000
 }
-mcts = MCTS(game, args)
+model = RestNet(game, 4, 64)
+model.eval()
+mcts = MCTS(game, args, model)
 
 while True:
     print(state)
