491b12fd21611259c57e08a9ea630699eb2a3513
TorenHF <toren@aduki.co.uk>
2024-07-26 12:13:46 +0200

    nn intergrated into mcts and optimmized. No eval yet

--- train.py
+++ train.py
@@ -7,53 +7,40 @@ from tqdm.notebook import trange
 import random
 torch.manual_seed(0)
 
-class Connect4:
+class Tictactoe:
     def __init__(self):
-        self.row_count = 6
-        self.colum_count = 7
-        self.action_size = self.colum_count
-        self.in_a_row = 4
+        self.row_count = 3
+        self.colum_count = 3
+        self.action_size = self.row_count * self.colum_count
 
     def __repr__(self):
-        return "Connect4"
+        return "Tictactoe"
 
     def get_init_state(self):
         return np.zeros((self.row_count, self.colum_count))
 
     def get_next_state(self, state, action, player):
-        row = np.max(np.where(state[:, action] == 0))
-        state[row, action] = player
-        return state
+        row = action // self.row_count
+        colum = action % self.colum_count
+        new_state = state.copy()  # Make a copy to avoid modifying the original state
+        new_state[row, colum] = player
+        return new_state
 
     def get_valid_moves(self, state):
-        return (state[0] == 0).astype(np.uint8)
+        return (state.reshape(-1) == 0).astype(np.uint8)
 
     def check_win(self, state, action):
-        if action == None:
+        if action is None:
             return False
-        row = np.min(np.where(state[:,action] !=0))
-        colum = action
-        player = state[row][colum]
-
-        def count(offset_row, offset_column):
-            for i in range(1, self.in_a_row):
-                r= row + offset_row*i
-                c = colum + offset_column*i
-                if (
-                    r<0
-                    or r>= self.row_count
-                    or c<0
-                    or c >= self.colum_count
-                    or state[r][c] !=player
-                ):
-                    return i -1
-            return self.in_a_row-1
+        row = action // self.row_count
+        colum = action % self.colum_count
+        player = state[row, colum]
 
         return (
-            count(1, 0)>= self.in_a_row-1
-            or (count(0,1)+ count(0, -1)) >= self.in_a_row-1
-            or (count(1,1)+ count(-1, -1)) >= self.in_a_row-1
-            or (count(1, -1)+ count(-1, -1)) >= self.in_a_row-1
+            np.sum(state[row, :]) == player * self.colum_count
+            or np.sum(state[:, colum]) == player * self.row_count
+            or np.sum(np.diag(state)) == player * self.row_count
+            or np.sum(np.diag(np.flip(state, axis=0))) == player * self.colum_count
         )
 
     def get_value_and_terminate(self, state, action):
@@ -76,12 +63,9 @@ class Connect4:
         encoded_state = np.stack(
             (state == -1, state == 0, state ==1)
         ).astype(np.float32)
-        if len(state.shape) == 3:
-            encoded_state = np.swapaxes(encoded_state, 0, 1)
-
         return encoded_state
 
-class ResNet(nn.Module):
+class RestNet(nn.Module):
     def __init__(self, game, num_resBlocks, num_hidden, device):
         super().__init__()
         self.device = device
@@ -212,71 +196,6 @@ class Node:
             self.parent.backpropagate(value)
 
 
-
-class MCTSParallel:
-    def __init__(self, game, args, model):
-        self.game = game
-        self.args = args
-        self.model = model
-
-    @torch.no_grad()
-    def search(self, states, spGames):
-        policy, _ = self.model(
-            torch.tensor(self.game.get_encoded_state(states), device=self.model.device)
-        )
-        policy = torch.softmax(policy, axis=1).cpu().numpy()
-        policy = ((1-self.args['dirichlet_epsilon']) * policy + self.args['dirichlet_epsilon']
-                  * np.random.dirichlet([self.args['dirichlet_alpha']]* self.game.action_size, size=policy.shape[0]))
-
-        for i, spg in enumerate(spGames):
-            spg_policy = policy[i]
-            valid_moves = self.game.get_valid_moves(state)
-            spg_policy *= valid_moves
-            spg_policy /= np.sum(spg_policy)
-
-            spg.root = Node(self.game, self.args, states[i], visit_count=1)
-
-            spg.root.expand(spg_policy)
-
-        for search in range(self.args['num_searches']):
-            for spg in spGames:
-                spg.node = None
-                node = spg.root
-
-                while node.is_fully_expanded():
-                    node = node.select()
-
-                value, is_terminal = self.game.get_value_and_terminate(node.state, node.action_taken)
-                value = self.game.get_opponent_value(value)
-
-                if is_terminal:
-                    node.backpropagate(value)
-
-                else:
-                    spg.node = node
-
-                expandable_spGames = [mappingIdx for mappingIdx in range(len(spGames)) if spGames[mappingIdx].node is not None]
-
-                if len(expandable_spGames) >0:
-                    states = np.stack([spGames[mappingIdx].node.state for mappingIdx in expandable_spGames])
-
-                    policy, value = self.model(
-                        torch.tensor(self.game.get_encoded_state(states), device=self.model.device)
-                    )
-                    policy = torch.softmax(policy, axis=1).cpu().numpy()
-                    value = value.cpu().numpy()
-
-                for i, mappingIdx in enumerate(expandable_spGames):
-                    spg_policy, spg_value = policy[i], value[i]
-                    node = spGames[mappingIdx].node
-
-                    valid_moves = self.game.get_valid_moves(node.state)
-                    spg_policy *= valid_moves
-                    spg_policy /= np.sum(spg_policy)
-
-                    node.expand(spg_policy)
-                    node.backpropagate(spg_value)
-
 class MCTS:
     def __init__(self, game, args, model):
         self.game = game
@@ -329,64 +248,42 @@ class MCTS:
             action_probs[child.action_taken] = child.visit_count
         action_probs /= np.sum(action_probs)
         return action_probs
-
-
-
-
-
-
-class AlphaZeroParallel:
+class AlphaZero:
     def __init__(self, model, optimizer, game, args):
         self.model = model
         self.optimizer = optimizer
         self.game = game
         self.args = args
-        self.mcts = MCTSParallel(game, args, model)
+        self.mcts = MCTS(game, args, model)
 
     def selfPlay(self):
-        return_memory = []
+        memory = []
         player = 1
-        spGames = (SPG(self.game) for  spg in range(self.args['num_parallel_games']))
+        state = self.game.get_init_state()
 
+        while True:
+            neutral_state = self.game.change_perspective(state, player)
+            action_probs = self.mcts.search(neutral_state)
+            memory.append((neutral_state, action_probs, player))
 
-        while len(spGames) >0:
-            states = np.stack([spg.state for spg in spGames])
+            temperature_action_probs = action_probs ** (1/ self.args['temperature'])
+            action = np.random.choice(self.game.action_size, p=action_probs)
 
-            neutral_states = self.game.change_perspective(states, player)
+            state = self.game.get_next_state(state, action, player)
 
-            self.mcts.search(neutral_states, spGames)
-
-            for i in range(len(spGames))[::-1]:
-                spg = spGames[i]
-
-                action_probs = np.zeros(self.game.action_size)
-                for child in spg.root.children:
-                    action_probs[child.action_taken] = child.visit_count
-                action_probs /= np.sum(action_probs)
-
-
-                spg.memory.append((spg.root.state, action_probs, player))
-
-                temperature_action_probs = action_probs ** (1/ self.args['temperature'])
-                action = np.random.choice(self.game.action_size, p=temperature_action_probs)
-
-                spg.state = self.game.get_next_state(spg.state, action, player)
-
-                value, is_terminal = self.game.get_value_and_terminate(spg.state, action)
-                if is_terminal:
-                    for hist_neutral_state, hist_action_probs, hist_player in spg.memory:
-                        hist_outcome = value if hist_player == player else self.game.get_opponent_value(value)
-                        return_memory.append((
-                            self.game.get_encoded_state(hist_neutral_state),
-                            hist_action_probs,
-                            hist_outcome
-                        ))
-                    del spGames[i]
-
+            value, is_terminal = self.game.get_value_and_terminate(state, action)
+            if is_terminal:
+                returnMemory = []
+                for hist_neutral_state, hist_action_probs, hist_player in memory:
+                    hist_outcome = value if hist_player == player else self.game.get_opponent_value(value)
+                    returnMemory.append((
+                        self.game.get_encoded_state(hist_neutral_state),
+                        hist_action_probs,
+                        hist_outcome
+                    ))
+                return returnMemory
 
             player = self.game.get_opponent(player)
-
-        return return_memory
 
     def train(self, memory):
         random.shuffle(memory)
@@ -405,50 +302,38 @@ class AlphaZeroParallel:
             value_loss = F.mse_loss(out_value, value_targets)
             loss = policy_loss + value_loss
 
-            self.optimizer.zero_grad()
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
 
     def learn(self):
-        for iteration in self.args['num_iterations']:
+        for iteration in range(self.args['num_iterations']):
+            print(f"iteration: {iteration}")
             memory = []
 
             self.model.eval()
-            for selfPlay_iteration in trange(self.args['num_selfPlay_iterations'] // self.args['num_parallel_games']):
+            for selfPlay_iteration in range(self.args['num_selfPlay_iterations']):
                 memory += self.selfPlay()
+                print(f"game: {selfPlay_iteration}")
 
             self.model.train()
-            for epoch in trange(self.args['num_epochs']):
+            for epoch in range(self.args['num_epochs']):
                 self.train(memory)
 
-            torch.save(self.model.state_dict(), f"model_{iteration}_{self.game}.pt" )
-            torch.save(self.optimizer.state_dict(), f"optimizer_{iteration}_{self.game}.pt")
+            torch.save(self.model.state_dict(), f"model_{iteration}.pt" )
+            torch.save(self.optimizer.state_dict(), f"optimizer_{iteration}.pt")
 
-
-class SPG:
-    def __int__(self, game):
-        state = game.get_init_state()
-        self.memory = []
-        self.root = None
-        self.node = None
-
-
-
-
-
-
-game = Connect4()
+tictactoe = Tictactoe()
 player = 1
-state = game.get_init_state()
+state = tictactoe.get_init_state()
 
 
 args = {
     'C': 2,
-    'num_searches': 600,
-    'num_iterations' : 8,
-    'num_selfPlay_iterations' : 500,
-    'num_parallel_games' : 100,
+    'num_searches': 100,
+    'num_iterations' : 6,
+    'num_selfPlay_iterations' : 300,
     'num_epochs' : 4,
     'batch_size' : 64,
     'temperature' : 1.25,
@@ -456,32 +341,33 @@ args = {
     'dirichlet_alpha' : 0.3
 
 }
-model = ResNet(game, 9, 128, device=torch.device("cpu"))
+model = RestNet(tictactoe, 4, 64, device=torch.device("cpu"))
 model.eval()
-mcts = MCTS(game, args, model)
-mcts_train = MCTSParallel(game, args, model)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+mcts = MCTS(tictactoe, args, model)
+device = torch.device("cuda" if torch.cuda.is_available else "cpu")
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
-alphazero = AlphaZeroParallel(model, optimizer, game, args)
+
+alphaZero = AlphaZero(model, optimizer, tictactoe, args)
 
 def play(state, player):
     while True:
         print(state)
         if player == 1:
-            valid_moves = game.get_valid_moves(state)
-            print("Valid moves:", [i for i in range(game.action_size) if valid_moves[i] == 1])
+            valid_moves = tictactoe.get_valid_moves(state)
+            print("Valid moves:", [i for i in range(tictactoe.action_size) if valid_moves[i] == 1])
             action = int(input(f"Player {player}, enter your move (0-8): "))
 
             if valid_moves[action] == 0:
                 print("Action not valid. Try again.")
                 continue
         else:
-            neutral_state = game.change_perspective(state, player)
+            neutral_state = tictactoe.change_perspective(state, player)
             mcts_probs = mcts.search(neutral_state)
             action = np.argmax(mcts_probs)
 
-        state = game.get_next_state(state, action, player)
-        value, is_terminal = game.get_value_and_terminate(state, action)
+
+        state = tictactoe.get_next_state(state, action, player)
+        value, is_terminal = tictactoe.get_value_and_terminate(state, action)
 
         if is_terminal:
             print(state)
@@ -491,5 +377,5 @@ def play(state, player):
                 print("It's a draw!")
             break
 
-        player = game.get_opponent(player)
-alphazero.learn()
+        player = tictactoe.get_opponent(player)
+play(state, player)
